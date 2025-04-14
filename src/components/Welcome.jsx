3e835ab01dd2bdbf603363163111f793571b9fbf
Jaystorm56 <olamike83@yahoo.com>
2025-04-14 12:46:15 +0100

    final welcom

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -12,16 +12,16 @@ const Welcome = ({ setActiveTab }) => {
 
   return (
     <div className="min-h-screen flex flex-col justify-between items-center text-center bg-background">
-      <Box sx={{}}>
-        <img src={meetzLogo} alt="Meetz Logo" className="h-20 mx-auto" />
+      <Box sx={{flex:1}}>
+        <img src={meetzLogo} alt="Meetz Logo" className="h-12 mx-auto" />
       </Box>
-      <Box sx={{ display: 'flex', alignItems: 'center',border:'3px solid',  justifyContent: 'center' }}>
+      <Box sx={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <video
           src={animatedVideo}
           autoPlay
           muted
           playsInline
-          className="max-w-full max-h-[55vh] object-contain"
+          className="max-w-full max-h-[60vh] object-contain"
         />
       </Box>
       <Typography variant="h4" className="welcome-text text-accent text-2xl font-extrabold" sx={{ fontSize: 25, fontWeight: 550 }}>Discover Love</Typography>
